e7cb8c85030628c26434ae58cbf0a5ca778a2620
Luluo-22 <luluoytb@gmail.com>
2025-10-23 13:02:25 +0200

    v.0.1 preguntar nombre y saludo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("¿Cual es tu nombre?");
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.next();
+        System.out.println("Encantando de conocerte " + nombre);
     }
 }
